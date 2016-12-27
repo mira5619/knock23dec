5c599184daf7af2f4b275f98ad25241167c8dd68
Margarita <ivanova5619@abv.bg>
2016-12-27 17:13:18 +0000

    added filter

--- js/script.js
+++ js/script.js
@@ -1,60 +1,56 @@
 $(document).ready(function() {
 
-        var repositories = [];
-        function loadData() {
-        // var ghdata = $('#ghdata');
-        // clear out old data before new request*/
-        // ghdata.text("");
 
-        var ghUrl = 'https://api.github.com/repositories?since=1; rel="first"';
-        // var sghUrl = 'https://api.github.com/users/' + repo_search;
+  var repositories = [];
+  function loadData() {
 
-        $.getJSON(ghUrl, function (json) {
+    var ghUrl = 'https://api.github.com/repositories?since=1; rel="first"';
+    // var sghUrl = 'https://api.github.com/users/' + repo_search;
 
-          for (var i = 0; i < json.length; i++) {
-                var repository = json[i];
-                repositories.push(repository);
-           }; //end of the loop
+    $.getJSON(ghUrl, function (json) {
+      for (var i = 0; i < json.length; i++) {
+        var repository = json[i];
+        repositories.push(repository);
+      }; //end of the loop
 
-    console.log(repositories);
+      //console.log(repositories);
+      var Repo = function(data){
+        this.name = data.name;
+        this.full_name = data.full_name;
+        this.user = data.owner.login;
+        this.user_repo = data.owner.html_url;
+        this.description = data.description;
+        this.languages = data.languages_url;
+        this.link_to_repository = data.html_url;
+      };
 
+      //********view model*******
 
-        var Repo = function(data){
-          this.name = data.name;
-          this.full_name = data.full_name;
-          this.user = data.owner.login;
-          this.user_repo = data.owner.html_url;
-          this.description = data.description;
-          this.languages = data.languages_url;
-          this.link_to_repository = data.html_url;
-        };
+      var ViewModel =  function (){
+        var self = this;
 
- //********view model*******
+        this.repoList = ko.observableArray([]);
 
-        var ViewModel =  function (){
-          var self = this;
+        repositories.forEach(function(repository) {
+          self.repoList.push(new Repo(repository));
+        });
 
-          this.repoList = ko.observableArray([]);
+        this.searchStr = ko.observable("");
 
-          repositories.forEach(function(repository) {
-            self.repoList.push(new Repo(repository));
+        this.search = ko.computed(function() {
+          return ko.utils.arrayFilter(self.repoList(), function(repo) {
+            if (repo.name.toLowerCase().indexOf(self.searchStr().toLowerCase())>=0) {
+              return true;
+            }
           });
-
-          // set first repo
-          this.currentRepo = ko.observable(this.repoList()[0]);
-          this.setRepo = function(clickedRepo) {
-            self.currentRepo(clickedRepo);
-          };
-
-        };
-
-        ko.applyBindings(new ViewModel());
-
-          }).error(function(e){
+        });
+      };
+      ko.applyBindings(new ViewModel());
+      }).error(function(e){
         ghdata.text('Github Repositories Could Not Be Loaded');
     });
 
-      }; //close loadData
-      $("#btn").on('click', loadData);
+  }; //close loadData
+  $("#btn").on('click', loadData);
 
-    }); //close document ready
+}); //close document ready
